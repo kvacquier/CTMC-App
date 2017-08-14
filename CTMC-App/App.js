abbaf21b7a9b8f11f0c62a8852ad9e9808a1b454
DES69U-FR <aymeric.deliencourt@nimes.yncrea.fr>
2017-08-14 10:19:15 +0200

    First App Test
     create-react-native-app : fichier de basse

---
+++ CTMC-App/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>CTMC App</Text>
+        <Text>Premier test app CTMC</Text>
+        <Text>BLABLABLA</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
