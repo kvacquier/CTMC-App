137550acb91176ab112dd307c6090a551a57eec7
Kevin VACQUIER <kvacquier@icloud.com>
2017-08-16 15:29:12 +0200

    News on homescreen

--- ctmc-app/screens/HomeScreen.js
+++ ctmc-app/screens/HomeScreen.js
@@ -9,118 +9,79 @@ import {
   View,
 } from 'react-native';
 import { WebBrowser } from 'expo';
+import HTMLView from 'react-native-htmlview';
 
 import { MonoText } from '../components/StyledText';
 
+var REQUEST_URL = 'http://mauguio-tir.fr/wp-json/wp/v2/pages/2'
+
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
-    header: null,
+    title: 'News',
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+        loaded: false,
+      };
+    }
+
+    componentDidMount() {
+      this.fetchData();
+    }
+  
+    fetchData() {
+      fetch(REQUEST_URL)
+        .then((response) => response.json())
+        .then((responseData) => {
+          this.setState({
+            page: responseData,
+            loaded: true,
+          });
+        })
+        .done();
+    }
+
   render() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
     return (
       <View style={styles.container}>
-        <ScrollView
-          style={styles.container}
-          contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer}>
-            <Image
-              source={
-                __DEV__
-                  ? require('../assets/images/robot-dev.png')
-                  : require('../assets/images/robot-prod.png')
-              }
-              style={styles.welcomeImage}
-            />
-          </View>
-
-          <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
-
-            <Text style={styles.getStartedText}>Get started by opening</Text>
-
-            <View
-              style={[
-                styles.codeHighlightContainer,
-                styles.HomeScreenFilename,
-              ]}>
-              <MonoText style={styles.codeHighlightText}>
-                screens/HomeScreen.js
-              </MonoText>
-            </View>
-
-            <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload.
-            </Text>
-          </View>
-
-          <View style={styles.helpContainer}>
-            <TouchableOpacity
-              onPress={this._handleHelpPress}
-              style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>
-                Help, it didn’t automatically reload!
-              </Text>
-            </TouchableOpacity>
-          </View>
+        <ScrollView>
+        <HTMLView
+          value={this.state.page.content.rendered}
+          stylesheet={styles}
+          addLineBreaks={false}
+        />
         </ScrollView>
-
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>
-            This is a tab bar. You can edit it in:
-          </Text>
-
-          <View
-            style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>
-              navigation/MainTabNavigator.js
-            </MonoText>
-          </View>
-        </View>
       </View>
     );
   }
 
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
+  renderLoadingView() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Chargement des Petites Annonces...
         </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use
-          useful development tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
+      </View>
+    );
   }
 
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/development-mode'
-    );
-  };
-
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-    );
-  };
+  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+    padding: 8,
   },
   developmentModeText: {
     marginBottom: 20,
