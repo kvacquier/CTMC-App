f1f96dc9997d527db4ab30e1dc938547bcafb563
DES69U-FR <aymeric.deliencourt@nimes.yncrea.fr>
2017-08-17 18:24:36 +0200

    Fix Font Error / Set Icon & Readme.md / Add Fonts

--- CTMC-App/App.js
+++ CTMC-App/App.js
@@ -40,7 +40,9 @@ export default class App extends React.Component {
           Ionicons.font,
           // We include SpaceMono because we use it in HomeScreen.js. Feel free
           // to remove this if you are not using it in your app
-          { 'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf') },
+          { 'Helvetica Neue': require('./assets/fonts/HelveticaNeue.ttf') },
+          { 'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf') }
+          { 'Slant': require('./assets/fonts/Slant.ttf') }
         ]),
       ]);
     } catch (e) {
